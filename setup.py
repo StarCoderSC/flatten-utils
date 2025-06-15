eefe7e54fd5e1ea37fdd9642cfd1f673f3065dab
StarCoderSC <lyfkodbdream@gmail.com>
2025-06-15 17:36:06 +0530

    Updated setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="flatten-utils",
-    version="0.1.2",
+    version="0.1.3",
     description="CLi + module to deeply flatten nested structures like a pro",
     long_description=long_description,
     long_description_content_type="text/markdown",
